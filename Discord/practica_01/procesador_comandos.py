815185ff9ff7c7dadd2b2c34ab23fa064f74b5f7
Alan90010 <dominzalan@gmail.com>
2026-04-28 07:56:52 -0600

    Avance

--- Discord/practica_01/procesador_comandos.py
+++ Discord/practica_01/procesador_comandos.py
@@ -1,11 +1,24 @@
 import datetime
 
 def obtener_saludo(nombre_bot):
-def procesar_comando_recordad(argumento):
+
+    return ("Hola, soy (nombre_bot) y estoy listo para ayudarte.")
+
+def procesar_comando_recordad(comando):
+    if not comando:
+        return "Error, falta el nombre. Use !recordar [nombre]"
+
+    return f"Entendido! Recordare el nombre: {comando}"
+
 def calcular_uptime(hora_inicio):
+    ahora = datetime.datetime.now()
+    differencia = ahora - hora_inicio
+    segundos = int(differencia.total_seconds())
+    return f"Tiempo de actividad: {segundos} segundos"
 def mostrar_ayuda():
 def iniciar_agente():
 
 def main():
+    obtener_saludo(Discordio)
 if __name__ == "__main":
     main()
